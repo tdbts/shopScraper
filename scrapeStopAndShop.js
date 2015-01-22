7a4122dd6e72b0305d1f401afd62233831540b52
tdbts <VinnySanchez87@gmail.com>
2015-01-22 14:55:24 -0500

    It ain't pretty yet, but I used async to get a working implementation of a Stop and Shop scrape

--- scrapeStopAndShop.js
+++ scrapeStopAndShop.js
@@ -1,5 +1,6 @@
 var request = require('request'), 
-	cheerio = require('cheerio');
+	cheerio = require('cheerio'), 
+	async = require('async');
 
 function scrapeStopAndShop(callback) {
 
@@ -29,9 +30,78 @@ function scrapeStopAndShop(callback) {
 
 			console.log("Found " + result.length + " pages!");
 			console.log(result);
+
+			callback(result);
 		}
+	});
+}
+
+function getProducts(pageID, callback) {
+
+	request("http://scapi.shoplocal.com/stopandshop/2012.2/json/getpromotionpagelistings.aspx?campaignid=5e018ae35636a4e2&storeid=2599015&pageid=" + pageID, function (err, resp, body) {
+		
+		if (err) {
+			return new Error("There was an error making the URL request.\n" + err);
+		}
+
+		if (!err && resp.statusCode === 200) {
+
+			//console.log(body);
+			var json = JSON.parse(body);
+
+			var result = [];
+
+			console.log(json.content.collection.data);
+
+			if (json.content.collection.data && json.content.collection.data.length > 0) {
+
+				json.content.collection.data.map(function (product) {
+					result.push({
+						productName: product.title, 
+						productDescription: product.description || "No Description Provided", 
+						productPrice: product.price + " " + product.pricequalifier, 
+						productImage: product.image
+					});
+				});
+			}
+
+
+			//console.log(result);
+			callback(null, result);
+
+		}			
+
 	});
 
 }
 
-scrapeStopAndShop();
+
+scrapeStopAndShop(function (resultsArray) {
+
+	var pageIDs = [], 
+        allPageContent = [];	
+
+
+	resultsArray.map(function (page) {
+		return pageIDs.push(page.pageID);
+	});
+
+	console.log(pageIDs);
+
+
+	async.map(pageIDs, getProducts, function (err, results) {
+		
+		if (err) {
+			return new Error("There was an error mapping over the page IDs!");
+		}
+
+		//allPageContent.push(results);
+		console.log("Results are: ");
+		console.log(results);
+		console.log(results.length);
+		console.log(results.map(function (page) {
+			return page.length;
+		}));
+	});
+
+});
